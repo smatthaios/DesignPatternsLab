d9c20e2d00ad003542263a967d5c463983feab8e
Stelios Matthaios <stelios.matthaios@gmail.com>
2017-10-17 00:31:54 +0300

    Coffee exercise solution

---
+++ structural/src/main/java/gr/regenerationcodingschool/exercises/decorateCoffee/CoffeeClient.java
@@ -0,0 +1,12 @@
+package gr.regenerationcodingschool.exercises.decorateCoffee;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CoffeeClient {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CoffeeClient.class);
+ 
+	public static void main(String args[]) {
+
+	}
+}
