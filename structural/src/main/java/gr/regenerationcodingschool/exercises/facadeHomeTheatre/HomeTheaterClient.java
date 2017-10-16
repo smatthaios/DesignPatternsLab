98b3a09a5fc6293797212fd79fdbf5060a7c7634
Stelios Matthaios <stelios.matthaios@gmail.com>
2017-10-17 01:18:49 +0300

    Stripped home theater client from solution

--- structural/src/main/java/gr/regenerationcodingschool/exercises/facadeHomeTheatre/HomeTheaterClient.java
+++ structural/src/main/java/gr/regenerationcodingschool/exercises/facadeHomeTheatre/HomeTheaterClient.java
@@ -1,17 +1,12 @@
 package gr.regenerationcodingschool.exercises.facadeHomeTheatre;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HomeTheaterClient {
-	public static void main(String[] args) {
-		Tuner tuner = new Tuner("Top-O-Line AM/FM Tuner");
-		DvdPlayer dvd = new DvdPlayer("Top-O-Line DVD Player");
-		CdPlayer cd = new CdPlayer("Top-O-Line CD Player");
-		Projector projector = new Projector("Top-O-Line Projector", dvd);
-		Screen screen = new Screen("Theater Screen");
- 
-		HomeTheaterFacade homeTheater = 
-				new HomeTheaterFacade(tuner, dvd, cd, projector, screen);
- 
-		homeTheater.playMovie("Raiders of the Lost Ark");
-		homeTheater.stopMovie();
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeTheaterClient.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Create the appropriate home theater object that implements the appropriate functionality");
+    }
 }
